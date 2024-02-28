51321313351ddf784050b88bfd136cb9564ac946
claesmk <44329220+claesmk@users.noreply.github.com>
2024-02-28 22:48:36 +0000

    Added test for us_ssn

--- test_team_null.py
+++ test_team_null.py
@@ -12,3 +12,26 @@ class TestTeamNull(unittest.TestCase):
 
     def test_us_ssn(self):
         """Test us_ssn functionality"""
+        # positive testcases
+        prefix = ['123', '245', '532']
+        mid = ['44', '55', '66']
+        suffix = ['6789', '2233', '3344']
+
+        test_cases = [f"My SSN is {p}-{m}-{s}" for p in prefix for m in mid for s in suffix]
+
+        for test_string in test_cases:
+            expected = 'My SSN is <US_SSN>'
+            actual = anonymize_text(test_string, ['US_SSN'])
+            self.assertEqual(expected, actual)
+
+        # negative testcase - this will not be replaced
+        test_string = 'My SSN is 123-45-6789'
+        expected = 'My SSN is 123-45-6789'
+        actual = anonymize_text(test_string, ['US_SSN'])
+        self.assertEqual(expected, actual)
+
+        # another negative testcase - this will not be replaced
+        test_string = 'My SSN is 245-00-6789'
+        expected = 'My SSN is 245-00-6789'
+        actual = anonymize_text(test_string, ['US_SSN'])
+        self.assertEqual(expected, actual)
