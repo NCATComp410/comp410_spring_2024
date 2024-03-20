1e41b2380c47722359ba29278517a8df94a6b87f
Jamtyful <88339067+Jamtyful@users.noreply.github.com>
2024-03-20 14:23:00 +0000

    Added test functionality for SG_NRIC_FIN

--- test_team_guardiansofpii.py
+++ test_team_guardiansofpii.py
@@ -24,3 +24,30 @@ class TestTeamGuardiansOfPII(unittest.TestCase):
 
     def test_sg_nric_fin(self):
         """Test sg_nric_fin functionality"""
+        # positive test case 1
+        positive_test_string = 'My NRIC number is S0000001I' #using the first public FIN of the first president of Singapore
+        positive_expected_string = 'My NRIC number is <SG_NRIC_FIN>'
+        positive_actual_string = anonymize_text(positive_test_string, {'SG_NRIC_FIN'})
+
+        self.assertEqual(positive_expected_string, positive_actual_string)
+
+        #positive test case 2
+        positive_test_string = 'S0000002G is my FIN' # public FIN of the first chief justice of Singapore
+        positive_expected_string = '<SG_NRIC_FIN> is my FIN'
+        positive_actual_string = anonymize_text(positive_test_string, {'SG_NRIC_FIN'})
+
+        self.assertEqual(positive_expected_string, positive_actual_string)
+
+        #negative test case 1
+        negative_test_string = 'My NRIC number is SOOOOOO1I' #replaced zeros with letter O's 
+        negative_expected_string = 'My NRIC number is SOOOOOO1I'
+        negative_actual_string = anonymize_text(negative_test_string, {'SG_NRIC_FIN'})
+
+        self.assertEqual(negative_expected_string, negative_actual_string)
+
+        #negative test case 2
+        negative_test_string = 'S000000G2 is my FIN' #switched the checksum character and the last digit
+        negative_expected_string = 'S000000G2 is my FIN'
+        negative_actual_string = anonymize_text(negative_test_string, {'SG_NRIC_FIN'})
+
+        self.assertEqual(negative_expected_string, negative_actual_string)
