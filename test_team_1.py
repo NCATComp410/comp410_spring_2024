1256f7d47dcd792bf5574ffdd63e50867847e6ee
JD84395 <157139248+JD84395@users.noreply.github.com>
2024-03-22 05:57:00 +0000

    Added test for Credit_Card

--- test_team_1.py
+++ test_team_1.py
@@ -12,6 +12,29 @@ class TestTeam1(unittest.TestCase):
 
     def test_credit_card(self):
         """Test credit_card functionality"""
+        
+        # positive test case
+        prefix = ['4111', '5105', '6011']
+        middle1 = ['1111', '1051', '0000']
+        middle2 = ['1111', '0510', '0000']
+        suffix = ['1111', '5100', '0004']
+        
+        for p in prefix:
+            for m1 in middle1:
+                for m2 in middle2:
+                    for s in suffix:
+
+                        test_string = 'My credit card is ' + p + '-' + m1 + '-' + m2 + '-' + s 
+                        expected = 'My credit card is <CREDIT_CARD>'
+        actual = anonymize_text(test_string, ['CREDIT_CARD'])
+        self.assertEqual(expected, actual)
+
+            # negative test case
+        test_string = 'My credit card is 4000-0000-0000-0001'
+        expected = 'My credit card is 4000-0000-0000-0001'
+        actual = anonymize_text(test_string, ['CREDIT_CARD'])
+        self.assertEqual(expected, actual)
+
 
     def test_ip_address(self):
         """Test ip_address functionality"""
