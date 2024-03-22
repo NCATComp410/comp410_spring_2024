5290ac2c23566dab9da0a87e772e0964e35cff38
jpow2 <145511956+jpow2@users.noreply.github.com>
2024-03-22 16:11:53 +0000

    Added test for uk_nhs

--- test_team_2.py
+++ test_team_2.py
@@ -21,6 +21,24 @@ class TestTeam2(unittest.TestCase):
 
     def test_uk_nhs(self):
         """Test uk_nhs functionality"""
+        # positive testcases
+        prefix = ['123', '245', '532']
+        mid = ['333', '444', '555']
+        suffix = ['6789', '2232', '4832']
+
+        # Generates a testcase by utting the prefix, mid, and suffix together
+        test_cases = [f"My NHS number is {p}-{m}-{s}" for p in prefix for m in mid for s in suffix]
+
+        for test_string in test_cases:
+            expected = 'My NHS number is <UK_NHS>'
+            actual = anonymize_text(test_string, ['UK_NHS'])
+            self.assertEqual(expected, actual)
+
+        # negative testcase - this will not be replaced
+        test_string = 'My NHS number is 123-456-7890'
+        expected = 'My NHS number is 123-456-7890'
+        actual = anonymize_text(test_string, ['UK_NHS'])
+        self.assertEqual(expected, actual)
 
     def test_it_passport(self):
         """Test it_passport functionality"""
